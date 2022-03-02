3545c75be32d6bff0062a676b11ea8cf429b40e0
abbappii <bappi142434@gmail.com>
2022-03-02 17:51:54 +0600

    add participants

--- base/models.py
+++ base/models.py
@@ -16,10 +16,10 @@ class Rooms(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-
+    participants = models.ManyToManyField(User, related_name='participants', blank=True)
     # participants 
-    updated = models.DateField(auto_now=True)
-    created = models.DateField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     # metaclass use korar karon hosse last value add krar por oita upore show korano
     class Meta:
@@ -32,9 +32,12 @@ class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Rooms, on_delete=models.CASCADE)
     body = models.TextField(max_length=200)
-    updated = models.DateField(auto_now=True)
-    created = models.DateField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-updated','-created']
+        
     def __str__(self):
         return self.body[0:50]
 
